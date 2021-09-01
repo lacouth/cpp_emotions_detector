1ee261d4c0e9da3e4fb843c30fc854c473f5c70a
lacouth <lacouth@gmail.com>
2021-09-01 14:43:20 -0300

    add *.caffemodel to .gitignore

--- src/face.h
+++ src/face.h
@@ -5,23 +5,24 @@
 #include <opencv2/imgproc.hpp>
 
 
-class Face{
-    public:
-        int _xLeftBottom;
-        int _xRightTop;
-        int _yLeftBottom;
-        int _yRightTop;
-        double _confidence;
-        cv::Mat _face;
-        std::string _emotion;
+struct Face{
+    
+    int _xLeftBottom;
+    int _xRightTop;
+    int _yLeftBottom;
+    int _yRightTop;
+    double _confidence;
+    cv::Mat _face;
+    std::string _emotion;
 
-        Face(){}
-        Face(int xLeftBottom, int xRightTop, int yLeftBottom, int yRightTop, cv::Mat face, std::string emotion);
-        Face(Face &other);
-        Face(Face &&other);
+    Face(){}
+    Face(int xLeftBottom, int xRightTop, int yLeftBottom, int yRightTop, cv::Mat face, std::string emotion);
+    Face(Face &other);
+    Face(Face &&other);
 
-        Face& operator=(Face &other);
-        Face& operator=(Face &&other);
+    Face& operator=(Face &other);
+    Face& operator=(Face &&other);
+    
 };
 
 #endif
